7840f568521527602bcd1b8ddc9dbab984fa6225
Agbeku Mawuli Joshua <jagbeku@pdsghana.com>
2019-11-20 16:04:05 +0000

    data layer overview and models

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,14 @@
+import 'package:meta/meta.dart';
+import '../../domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        text: json['text'], number: (json['number'] as num).toInt());
+  }
+}
