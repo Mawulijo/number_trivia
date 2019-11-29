0265585ad7b8fab49eef55d22f590ffb4190081d
Agbeku Mawuli Joshua <jagbeku@ecggh.com>
2019-11-29 16:20:13 +0000

    Bloc Implementation 1/2

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -1,10 +1,28 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:number_trivia/core/presentation/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 import './bloc.dart';
+import 'package:meta/meta.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {@required getConcreteNumberTrivia concrete,
+      @required getRandomNumberTrivia random,
+      @required this.inputConverter})
+      : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
   @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
---
+++ test/features/number_trivia/presentation/number_trivia_bloc_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/presentation/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/bloc/bloc.dart';
+
+class MockGetConcreteNumberTrivia extends Mock
+    implements GetConcreteNumberTrivia {}
+
+class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  NumberTriviaBloc bloc;
+  MockGetConcreteNumberTrivia mockGetConcreteNumberTrivia;
+  MockGetRandomNumberTrivia mockGetRandomNumberTrivia;
+  MockInputConverter mockInputConverter;
+
+  setUp(() {
+    mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+    mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+    mockInputConverter = MockInputConverter();
+
+    bloc = NumberTriviaBloc(
+      concrete: mockGetConcreteNumberTrivia,
+      random: mockGetRandomNumberTrivia,
+      inputConverter: mockInputConverter,
+    );
+  });
+  test('initiatedState should be Empty', () {
+    //assert
+    expect(bloc.initialState, equals(Empty()));
+  });
+}
