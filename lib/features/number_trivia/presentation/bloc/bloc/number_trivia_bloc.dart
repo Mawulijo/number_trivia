c14b2e99c770f9c3d6902f22f4a07f7827492f97
mawulijo <mawulidickson@gmail.com>
2019-11-29 22:02:28 +0000

    Bloc Implementation 1/2

--- lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/bloc/number_trivia_bloc.dart
@@ -6,14 +6,19 @@ import 'package:number_trivia/features/number_trivia/domain/use_cases/get_random
 import './bloc.dart';
 import 'package:meta/meta.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
   final GetRandomNumberTrivia getRandomNumberTrivia;
   final InputConverter inputConverter;
 
   NumberTriviaBloc(
-      {@required getConcreteNumberTrivia concrete,
-      @required getRandomNumberTrivia random,
+      {@required GetConcreteNumberTrivia concrete,
+      @required GetRandomNumberTrivia random,
       @required this.inputConverter})
       : assert(concrete != null),
         assert(random != null),
@@ -28,6 +33,19 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
   ) async* {
-    // TODO: Add Logic
+    // Immediately branching the logic with type checking, in order
+    // for the event to be smart casted
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        // Although the "success case" doesn't interest us with the current test,
+        // we still have to handle it somehow.
+        (integer) => throw UnimplementedError(),
+      );
+    }
   }
 }
--- test/features/number_trivia/presentation/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/number_trivia_bloc_test.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia/core/presentation/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/bloc/bloc.dart';
@@ -32,5 +34,39 @@ void main() {
   test('initiatedState should be Empty', () {
     //assert
     expect(bloc.initialState, equals(Empty()));
+  });
+  group('GetTriviaForConcreteNumber', () {
+    // Thte event takes in a string
+    final tNumberString = '1';
+    // This is the successful output of the InputConverter
+    final tNumberParsed = int.parse(tNumberString);
+    // NumberTrivia instance is needed too
+    final tNumberTrivia = NumberTrivia(number: 1, text: 'test trivia');
+    test(
+        'should call the Inputconverter to validate and convert string to an unsigned integer',
+        () async {
+      //arrange
+      when(mockInputConverter.stringToUnsignedInteger(any))
+          .thenReturn(Right(tNumberParsed));
+      //act
+      bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+      await untilCalled(mockInputConverter.stringToUnsignedInteger(any));
+      //assert
+      verify(mockInputConverter.stringToUnsignedInteger(tNumberString));
+    });
+    test('should emit [error] when the input is invalid', () async {
+      //arrange
+      when(mockInputConverter.stringToUnsignedInteger(any))
+          .thenReturn(Left(InvalidInputFailure()));
+      //assert later
+      final expected = [
+        //The inintial state is always emitted first
+        Empty(),
+        Error(message: INVALID_INPUT_FAILURE_MESSAGE)
+      ];
+      expectLater(bloc.state, emitsInOrder(expected));
+      //act
+      bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+    });
   });
 }
