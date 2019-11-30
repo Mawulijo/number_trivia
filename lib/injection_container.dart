ca58f9cbcfbd731e7847ebef077b3e250e99cc03
mawulijo <mawulidickson@gmail.com>
2019-11-30 17:07:48 +0000

    Dependency Injection & User Interface

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,17 +1,17 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'core/network/network_info.dart';
 import 'core/presentation/util/input_converter.dart';
 import 'features/number_trivia/data/repositories/number_trivia_local_data_source.dart';
 import 'features/number_trivia/data/repositories/number_trivia_remote_data_source.dart';
 import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
 import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
 import 'features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 import 'features/number_trivia/presentation/bloc/bloc.dart';
-import 'package:http/http.dart' as http;
-import 'package:data_connection_checker/data_connection_checker.dart';
 
 final sl = GetIt.instance; //sl is for service locator
 
