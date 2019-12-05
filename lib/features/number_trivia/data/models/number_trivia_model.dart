7cd66029c36d75dc8732b5276dd57d2dc0be9144
Agbeku Mawuli Joshua <jagbeku@ecggh.com>
2019-12-05 09:25:07 +0000

    using the json_annotation package

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,21 +1,29 @@
 import 'package:meta/meta.dart';
 import '../../domain/entities/number_trivia.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'number_trivia_model.g.dart';
+
+@JsonSerializable()
 class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({
     @required String text,
     @required int number,
   }) : super(text: text, number: number);
 
-  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
-    return NumberTriviaModel(
-        text: json['text'], number: (json['number'] as num).toInt());
-  }
+  // factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+  //   return NumberTriviaModel(
+  //       text: json['text'], number: (json['number'] as num).toInt());
+  // }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'text': text,
-      'number': number,
-    };
-  }
+  // using the json_annotation package
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) =>
+      _$NumberTriviaModelFromJson(json);
+
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'text': text,
+
+  // using the json_annotation package
+  Map<String, dynamic> toJson() => _$NumberTriviaModelToJson(this);
 }
---
+++ lib/features/number_trivia/data/models/number_trivia_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'number_trivia_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NumberTriviaModel _$NumberTriviaModelFromJson(Map<String, dynamic> json) {
+  return NumberTriviaModel(
+      text: json['text'] as String, number: json['number'] as int);
+}
+
+Map<String, dynamic> _$NumberTriviaModelToJson(NumberTriviaModel instance) =>
+    <String, dynamic>{'text': instance.text, 'number': instance.number};
